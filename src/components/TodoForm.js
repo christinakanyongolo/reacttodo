8df60ca43f75c5e16bd50f97c410286a52133db1
christinakanyongolo <christina.w.kanyongolo@vanderbilt.edu>
2025-08-05 16:53:07 -0400

    failed attempt at dark mode

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,22 +1,23 @@
 import React, { useState } from 'react';
 import { TodoWrapper } from './TodoWrapper';
 
-export const TodoForm = ({ addTodo }) => {
-    const [value, setValue] = useState("");
+    export const TodoForm = ({ addTodo }) => {
+        const [value, setValue] = useState("");
 
-    const handleSubmit = (e ) => {
-        console.log("Submitting Todo");
-        console.log("addtodo", addTodo);
-        e.preventDefault( );
-        addTodo(value); 
-        setValue("")
+        const handleSubmit = (e ) => {
+            console.log("Submitting Todo");
+            console.log("addtodo", addTodo);
+            e.preventDefault( );
+            addTodo(value); 
+            setValue("")
 
-    } 
+        } 
 
-    return(   
-            <form className ="TodoForm" onSubmit = {handleSubmit}>
-                <input type = "text"  className ="TodoInput" placeholder="Take Over the World..."  value = {value} onChange = {(e) => setValue(e.target.value)}/>
-                <button type="submit" className="TodoButton">Add  Task</button>
-            </form>
+        return(   
+                <form className ="TodoForm" onSubmit = {handleSubmit}>
+                    <input type = "text"  className ="TodoInput" placeholder="Take Over the World..."  value = {value} onChange = {(e) => setValue(e.target.value)}/>
+                    <button type="submit" className="TodoButton">Add  Task</button>
+                </form>
 
-    )}
+        )}
+
---
+++ src/components/light/dark/darkmode.js
@@ -0,0 +1,20 @@
+let darkmode = localStorage.getIten('darkmode')
+const themeSwitch = document.getElementById('theme-switch')
+
+enableDarkmode = () => {
+    document.body.classlist.add('darkmode')
+    localStorage.setItem('darkmode', 'active')
+}
+
+disableDarkmode = () => {
+    document.body.classlist.remove('darkmode')
+    localStorage.setItem('darkmode', null)
+
+}
+
+
+themeSwitch.addEventListener("click", ()=> {
+    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+})
+
+

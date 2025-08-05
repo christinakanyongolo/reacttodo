723fee2472866b99e2219c5dd6a75925cc60c660
christinakanyongolo <christina.w.kanyongolo@vanderbilt.edu>
2025-08-05 18:07:34 -0400

    semiii got dark mode to work

--- public/darkmode.js
+++ public/darkmode.js
@@ -1,17 +1,19 @@
-
-let darkmode = localStorage.getIten('darkmode')
+let darkmode = localStorage.getItem('darkmode')
 const themeSwitch = document.getElementById('theme-switch')
 
 enableDarkmode = () => {
-    document.body.classlist.add('darkmode')
+    document.body.classList.add('darkmode')
     localStorage.setItem('darkmode', 'active')
 }
 
 disableDarkmode = () => {
-    document.body.classlist.remove('darkmode')
+    document.body.classList.remove('darkmode')
     localStorage.setItem('darkmode', null)
 
 }
+
+if (darkmode === "active") enableDarkmode()
+darkmode = localStorage.getItem('darkmode')
 
 
 themeSwitch.addEventListener("click", ()=> {

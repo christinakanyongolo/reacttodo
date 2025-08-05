027ba4bcf1377676747bc10a165a95a68be10a1f
christinakanyongolo <christina.w.kanyongolo@vanderbilt.edu>
2025-08-05 00:02:49 -0400

    tried and failed to debug addTodo

---
+++ src/components/Todo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+export const Todo = () => {
+    return (
+        <div>  className = "EditTodoForm"  
+            <p> Go to school</p>
+       </div> 
+          
+
+    ) }
---
+++ src/components/TodoForm.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+
+export const TodoForm = ({ addTodo }) => {
+    const [value, setValue] = useState("");
+
+    const handleSubmit = (e ) => {
+        console.log("Submitting Todo");
+        console.log("addtodo", addTodo);
+        e.preventDefault( );
+        addTodo(value); 
+        setValue("")
+
+    } 
+
+    return(   
+            <form className ="TodoForm" onSubmit = {handleSubmit}>
+                <input type = "text"  className ="TodoInput" placeholder="Take Over the World..."  value = {value} onChange = {(e) => setValue(e.target.value)}/>
+                <button type="submit" className="TodoButton">Add  Task</button>
+            </form>
+
+    )}
